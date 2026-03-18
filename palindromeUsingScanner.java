338a51e10e44fd87b60cee251ee21e67154fc3f0
Sami Uddin <pieceofcode0@gmail.com>
2026-03-18 14:29:42 +0000

    Add palindrome task

---
+++ palindromeUsingScanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class palindromeUsingScanner {
+    public static void main(String[] arr) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string to check if palindrome:");   
+        String input = scanner.nextLine();
+        String reversed = new StringBuilder(input).reverse().toString();
+        System.out.println("Reversed string: " + reversed);
+        
+        if (input.equalsIgnoreCase(reversed)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+
+}
+
