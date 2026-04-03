e6e17c5c483a540f2d0c1d580079895c8568efbe
S <57012818-pieceofcode0@users.noreply.replit.com>
2026-04-03 04:40:49 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -1,6 +1,6 @@
 { pkgs }: {
     deps = [
-        pkgs.graalvm17-ce
+        pkgs.openjdk17-bootstrap
         pkgs.maven
         pkgs.replitPackages.jdt-language-server
         pkgs.replitPackages.java-debug
